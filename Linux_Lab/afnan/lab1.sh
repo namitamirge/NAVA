c765bb37e227ac8186e34b5f2d22b86b56d6f0b0
Ubuntu <ubuntu@ip-172-31-29-162.us-west-2.compute.internal>
2023-02-24 19:30:30 +0000

    lab 1

---
+++ Linux_Lab/afnan/lab1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+read -p "Hello, what is your name? " name
+echo ""
+echo "It is a pleasure to meet you ${name}!"
+echo "Let's get to know each other better!"
+echo ""
+read -p "What is you favorite color? " color
+echo "That's interesting, my favorite color is also ${color}!"
