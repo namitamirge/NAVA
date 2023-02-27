d90136f51a00efe861b0d399a12576a41246d7b6
devvipra <dev.vaggarwal@gmail.com>
2023-02-26 22:00:33 -0600

    add new code

---
+++ Linux_vip/game.sh
@@ -0,0 +1,34 @@
+
+#!/bin/bash
+
+linux() {
+         
+  echo "which command do you want to know?"
+read command
+                 if [ $command == ls ]; then
+                 echo "list all the files and directory "
+       elif [ $command == cd ]; then
+        echo "change directory"
+       elif [ $command == touch ]; then
+         echo " To create files"
+        elif [ $command == mkdir ]; then
+          echo " To create directory"
+        else
+          echo "Sorry, Command not found
+"
+        fi
+}
+echo "Which language do you want to Learn?"
+read  language
+lowerstr=$(echo "$language" | tr '[:upper:]' '[:lower:]')
+ 
+ if [ $lowerstr == linux ]; then
+
+linux
+elif [ $lowerstr == python ]; then
+echo "Sorry, Language not supported now"
+else
+echo "Sorry, Wrong Entry!!"
+fi
+  
+ 
